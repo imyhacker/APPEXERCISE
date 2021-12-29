1fb085d7777244fe8372b4c2a9cc9d6a7e970f5c
imyhacker <arifarhan1602@gmail.com>
2021-12-29 08:49:55 +0700

    got

--- vendor/arikun/stislasimple/core/views/components/logo.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="login-brand">
-              <img src="https://demo.getstisla.com/assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
-            </div>
